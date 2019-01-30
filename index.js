b3b5fd6633c69526f9f28a0c6c9ca2622d13c657
Austin Williams <austinq.williams@gmail.com>
2019-01-30 21:34:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function shout(string) {
   return string.toUpperCase()
 }
 function  whisper(string) {
-  return string ("toUpperCase")
+  return string.toUpperCase()
 }
 function sayHiToGrandma(string) {
  return "YES INDEED!" 
