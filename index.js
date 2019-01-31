f64763975da1a1accf74e58169eb70736e300a2d
Austin Williams <austinq.williams@gmail.com>
2019-01-31 00:12:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ function sayHiToGrandma(string) {
  else if (string === string.toUpperCase())
  return "YES INDEED!";
  else if (string === "I love you, Grandma.")
- return 
+ return "I love you, too."
 }
